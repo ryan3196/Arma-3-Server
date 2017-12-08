6c5674a4dc8e92aa97e61edc19db2e1a0ded704b
AkToker <34366112+AkToker@users.noreply.github.com>
2017-12-08 14:05:50 -0900

    Cup heli additions

--- mpmissions/Exile.Tanoa/Vehicles/Unarmed_Air/Rotor_Wing/prices.hpp
+++ mpmissions/Exile.Tanoa/Vehicles/Unarmed_Air/Rotor_Wing/prices.hpp
@@ -3,8 +3,6 @@
 ///////////////////////////////////////////////////////////////////////////////
 class Exile_Chopper_Huey_Green { quality = 2; price = 21000; };
 class Exile_Chopper_Huey_Desert { quality = 2; price = 21000; };
-class Exile_Chopper_Huey_Armed_Green { quality = 2; price = 25000; };
-class Exile_Chopper_Huey_Armed_Desert { quality = 2; price = 25000; };
 
 ///////////////////////////////////////////////////////////////////////////////
 // Hellcat
@@ -67,3 +65,31 @@ class Exile_Chopper_Taru_Black { quality = 4; price = 33000; };
 
 class Exile_Chopper_Taru_Covered_CSAT { quality = 4; price = 43000; };
 class Exile_Chopper_Taru_Covered_Black { quality = 4; price = 43000; };
+
+///////////////////////////////////////////////////////////////////////////////
+// Cup Helicopters
+///////////////////////////////////////////////////////////////////////////////
+class CUP_B_UH60M_Unarmed_US                                            { quality = 4; price = 40000; };
+class CUP_B_UH60L_Unarmed_US                                            { quality = 4; price = 40000; };
+class CUP_B_UH60M_Unarmed_FFV_US                                        { quality = 4; price = 40000; }
+class CUP_B_UH60L_Unarmed_FFV_US                                        { quality = 4; price = 40000; };
+class CUP_B_UH60M_Unarmed_FFV_MEV_US                                                { quality = 4; price = 40000; };
+class CUP_B_UH60L_Unarmed_FFV_MEV_US                                                { quality = 4; price = 40000; };
+class CUP_B_MH6J_USA                                                { quality = 3; price = 25000; };
+class CUP_B_AW159_Unarmed_GB                                                { quality = 4; price = 45000; };
+class CUP_B_Wildcat_Unarmed_RN_Blackcat                                                { quality = 4; price = 45000; };
+class CUP_B_MI6A_CDF                                                { quality = 4; price = 60000; };
+class CUP_B_MI6T_CDF                                                { quality = 4; price = 60000; };
+class CUP_B_Mi17_VIV_CDF                                                { quality = 4; price = 50000; };
+class CUP_B_Mi17_medevac_CDF                                                { quality = 4; price = 50000; };
+class CUP_B_CH53E_USMC                                                { quality = 4; price = 60000; };
+class CUP_B_CH53E_VIV_USMC                                                { quality = 4; price = 60000; };
+class CUP_B_CH53E_GER                                                { quality = 4; price = 60000; };
+class CUP_B_CH53E_VIV_GER                                                { quality = 4; price = 60000; };
+class CUP_B_Merlin_HC3_GB                                                { quality = 4; price = 50000; };
+class CUP_B_Merlin_HC3_VIV_GB                                                { quality = 4; price = 50000; };
+class CUP_B_Merlin_HC3A_GB                                                { quality = 4; price = 50000; };
+class CUP_B_Merlin_HC4_GB                                                { quality = 4; price = 50000; };
+class CUP_B_UH1Y_UNA_USMC                                                { quality = 2; price = 30000; };
+class CUP_B_UH1Y_MEV_USMC                                                { quality = 2; price = 30000; };
+class CUP_B_AH6X_USA                                                { quality = 3; price = 25000; };
