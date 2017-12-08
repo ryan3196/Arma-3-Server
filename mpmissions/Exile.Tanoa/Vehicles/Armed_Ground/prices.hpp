d4266a3ddcc7be969be53f33c3f4116308e82a29
Zataralee <Zataralee@gmail.com>
2017-12-08 03:24:07 -0800

    014

--- mpmissions/Exile.Tanoa/Vehicles/Armed_Ground/prices.hpp
+++ mpmissions/Exile.Tanoa/Vehicles/Armed_Ground/prices.hpp
@@ -1,10 +1,40 @@
+///////////////////////////////////////////////////////////////////////////////
+// Offroad (Armed)
+///////////////////////////////////////////////////////////////////////////////
+class Exile_Car_Offroad_Armed_Guerilla01 { quality = 5; price = 25000; };
+class Exile_Car_Offroad_Armed_Guerilla02 { quality = 5; price = 25000; };
+class Exile_Car_Offroad_Armed_Guerilla03 { quality = 5; price = 25000; };
+class Exile_Car_Offroad_Armed_Guerilla04 { quality = 5; price = 25000; };
+class Exile_Car_Offroad_Armed_Guerilla05 { quality = 5; price = 25000; };
+class Exile_Car_Offroad_Armed_Guerilla06 { quality = 5; price = 25000; };
+class Exile_Car_Offroad_Armed_Guerilla07 { quality = 5; price = 25000; };
+class Exile_Car_Offroad_Armed_Guerilla08 { quality = 5; price = 25000; };
+class Exile_Car_Offroad_Armed_Guerilla09 { quality = 5; price = 25000; };
+class Exile_Car_Offroad_Armed_Guerilla10 { quality = 5; price = 25000; };
+class Exile_Car_Offroad_Armed_Guerilla11 { quality = 5; price = 25000; };
+class Exile_Car_Offroad_Armed_Guerilla12 { quality = 5; price = 25000; };
+
 
 ///////////////////////////////////////////////////////////////////////////////
-// Example of Catigory Lable Here
+// SUV (Armed)
 ///////////////////////////////////////////////////////////////////////////////
+class Exile_Car_SUV_Armed_Black { quality = 4; price = 25000; };
 
-class Example_Vehicle_Class_Name { quality = 6; price = 100000; };
-class CUP_B_C130J_Cargo_USMC { quality = 6; price = 100000; };
-class CUP_B_C130J_GB { quality = 6; price = 100000; };
+///////////////////////////////////////////////////////////////////////////////
+// BRDM2
+///////////////////////////////////////////////////////////////////////////////
+class Exile_Car_BRDM2_HQ { quality = 5; price = 25000; };
 
-	
+///////////////////////////////////////////////////////////////////////////////
+// BTR40
+///////////////////////////////////////////////////////////////////////////////
+class Exile_Car_BTR40_MG_Green { quality = 6; price = 38000; };
+class Exile_Car_BTR40_MG_Camo { quality = 6; price = 38000; };
+
+///////////////////////////////////////////////////////////////////////////////
+// HMMWV
+///////////////////////////////////////////////////////////////////////////////
+class Exile_Car_HMMWV_M134_Green { quality = 5; price = 33000; };
+class Exile_Car_HMMWV_M134_Desert { quality = 5; price = 33000; };
+class Exile_Car_HMMWV_M2_Green { quality = 6; price = 40000; };
+class Exile_Car_HMMWV_M2_Desert { quality = 6; price = 40000; };
