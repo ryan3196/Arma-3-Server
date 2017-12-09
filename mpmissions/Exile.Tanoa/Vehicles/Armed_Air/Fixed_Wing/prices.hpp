ca80c722052dfc5e9d7147bb85369983fc7ad90a
AkToker <34366112+AkToker@users.noreply.github.com>
2017-12-08 15:47:32 -0900

    Armed cup fixed wing

--- mpmissions/Exile.Tanoa/Vehicles/Armed_Air/Fixed_Wing/prices.hpp
+++ mpmissions/Exile.Tanoa/Vehicles/Armed_Air/Fixed_Wing/prices.hpp
@@ -1,8 +1,42 @@
-
+// Completed by: AkToker 12/8/17
 ///////////////////////////////////////////////////////////////////////////////
-// Example of Catigory Lable Here
+// Cup Armed Planes
 ///////////////////////////////////////////////////////////////////////////////
 
-class Example_Vehicle_Class_Name { quality = 6; price = 100000; };
-class CUP_B_C130J_Cargo_USMC { quality = 6; price = 100000; };
-class CUP_B_C130J_GB { quality = 6; price = 100000; };
+class CUP_B_F35B_AA_USMC                                                { quality = 6; price = 700000; };
+class CUP_B_F35B_CAS_USMC                                               { quality = 6; price = 700000; };
+class CUP_B_F35B_LGB_USMC                                               { quality = 6; price = 700000; };
+class CUP_B_F35B_AA_BAF                                                 { quality = 6; price = 700000; };
+class CUP_B_F35B_CAS_BAF                                                { quality = 6; price = 700000; };
+class CUP_B_F35B_LGB_BAF                                                { quality = 6; price = 700000; };
+class CUP_B_L39_RKT_CZ                                                  { quality = 6; price = 550000; };
+class CUP_B_L39_BMB_CZ                                                  { quality = 6; price = 550000; };
+class CUP_B_SU34_LGB_CDF                                                { quality = 6; price = 700000; };
+class CUP_B_SU34_AGM_CDF                                                { quality = 6; price = 700000; };
+class CUP_B_A10_CAS_USA                                                 { quality = 6; price = 850000; };
+class CUP_B_A10_AT_USA                                                  { quality = 6; price = 850000; };
+class CUP_B_AV8B_CAP_USMC                                               { quality = 6; price = 800000; };
+class CUP_B_AV8B_GBU12_USMC                                             { quality = 6; price = 800000; };
+class CUP_B_AV8B_MK82_USMC                                              { quality = 6; price = 800000; };
+class CUP_B_AV8B_AGM_USMC                                               { quality = 6; price = 800000; };
+class CUP_B_GR9_CAP_GB                                        	        { quality = 6; price = 700000; };
+class CUP_B_GR9_Mk82_GB                                         	    { quality = 6; price = 700000; };
+class CUP_B_GR9_GBU12_GB                                                { quality = 6; price = 700000; };
+class CUP_B_GR9_AGM_GB                                                  { quality = 6; price = 700000; };
+class CUP_B_Su25_CDF                    	                            { quality = 6; price = 600000; };
+class CUP_B_MV22_USMC_RAMPGUN                                                { quality = 5; price = 300000; };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
