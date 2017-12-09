e854f208599a4054bc05a8683c66ce7603c60ca2
Zataralee <Zataralee@gmail.com>
2017-12-09 08:02:22 -0800

    Added Items Folders

---
+++ mpmissions/Exile.Tanoa/Items/Armory/LMGs/traders.hpp
@@ -0,0 +1,12 @@
+
+
+class Tanks
+	{
+		name = "Tanks";
+		icon = "a3\ui_f\data\gui\Rsc\RscDisplayArsenal\itemacc_ca.paa";
+		items[] = 
+		{
+			"Example_Class_Name_of_Vehicle_1",
+			"Example_Class_Name_of_Vehicle_2" //Notice there is not comma on the last item in this list.
+		};
+	};
---
+++ mpmissions/Exile.Tanoa/Items/Equipment/FirstAid/prices.hpp
@@ -0,0 +1,9 @@
+
+	///////////////////////////////////////////////////////////////////////////////
+	// Example of Catigory Lable Here
+	///////////////////////////////////////////////////////////////////////////////
+	
+        class Example_Vehicle_Class_Name                                	    { quality = 6; price = 100000; }; 
+        class CUP_B_C130J_Cargo_USMC                                            { quality = 6; price = 100000; };
+        class CUP_B_C130J_GB                                 	                { quality = 6; price = 100000; };
+        
