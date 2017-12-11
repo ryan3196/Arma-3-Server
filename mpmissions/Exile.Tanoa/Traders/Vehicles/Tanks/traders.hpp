504aed800573001df3a3ba520d97a5ad41d6e77d
Zataralee <Zataralee@gmail.com>
2017-12-11 01:02:02 -0800

    Tanks Complete Traders

--- mpmissions/Exile.Tanoa/Traders/Vehicles/Tanks/traders.hpp
+++ mpmissions/Exile.Tanoa/Traders/Vehicles/Tanks/traders.hpp
@@ -6,7 +6,39 @@ class Tanks
 		icon = "a3\ui_f\data\gui\Rsc\RscDisplayArsenal\itemacc_ca.paa";
 		items[] = 
 		{
-			"Example_Class_Name_of_Vehicle_1",
-			"Example_Class_Name_of_Vehicle_2" //Notice there is not comma on the last item in this list.
+			"CUP_B_LAV25_USMC",
+			"CUP_B_LAV25M240_USMC",
+			"CUP_B_LAV25_HQ_USMC",
+			"CUP_B_M1126_ICV_M2_Desert",
+			"CUP_B_M1126_ICV_M2_Woodland",
+			"CUP_B_M1126_ICV_MK19_Desert",
+			"CUP_B_M1126_ICV_MK19_Woodland",
+			"CUP_B_M1130_CV_M2_Desert",
+			"CUP_B_M1130_CV_M2_Woodland",
+			"CUP_B_M1135_ATGMV_Desert",
+			"CUP_B_M1135_ATGMV_Woodland",
+			"CUP_B_M1128_MGS_Desert",
+			"CUP_B_M1128_MGS_Woodland",
+			"CUP_B_AAV_USMC",
+			"CUP_B_FV432_Bulldog_GB_D",
+			"CUP_B_FV432_Bulldog_GB_D_RWS",
+			"CUP_B_FV510_GB_D_SLAT",
+			"CUP_B_FV510_GB_D",
+			"CUP_B_M113_USA",
+			"CUP_B_M163_USA",
+			"CUP_B_MCV80_GB_D_SLAT",
+			"CUP_B_MCV80_GB_D",
+			"CUP_B_M2Bradley_USA_D",
+			"CUP_B_M2A3Bradley_USA_D",
+			"CUP_B_M2A3Bradley_USA_W",
+			"CUP_B_M7Bradley_USA_D",
+			"CUP_B_M6LineBacker_USA_D",
+			"CUP_B_M1A1_Woodland_US_Army",
+			"CUP_B_M1A1_DES_US_Army",
+			"CUP_B_M1A2_TUSK_MG_US_Army",
+			"CUP_B_M1A2_TUSK_MG_DES_US_Army",
+			"CUP_B_T72_CDF",
+			"CUP_B_T72_CZ",
+			"CUP_B_ZSU23_CDF"
 		};
 	};
