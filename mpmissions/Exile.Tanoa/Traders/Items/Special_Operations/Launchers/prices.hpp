caafae6ce936460f04c86969260e634488f97a6c
Mike-Hawk <rdbriner3196@sbcglobal.net>
2017-12-14 00:09:47 -0500

    Added More Launchers. Changed Respect Level -Mike

--- mpmissions/Exile.Tanoa/Traders/Items/Special_Operations/Launchers/prices.hpp
+++ mpmissions/Exile.Tanoa/Traders/Items/Special_Operations/Launchers/prices.hpp
@@ -1,14 +1,21 @@
-//CUP Launchers
+//Vanilla_AA
+class launch_B_Titan_F { quality = 5; price = 5000; };
+class launch_B_Titan_tna_F { quality = 5; price = 5000; };
+  
+//Vanilla_AT
+class launch_NLAW_F { quality = 5; price = 5000; };
+class launch_RPG7_F { quality = 5; price = 5000; };
+class launch_RPG32_F { quality = 5; price = 5000; };
 
 // CUP_AA
-class CUP_launch_Igla { quality = 6; price = 5000; };
-class CUP_launch_M136 { quality = 6; price = 5000; };
-class CUP_launch_9K32Strela { quality = 6; price = 5000; };
+class CUP_launch_Igla { quality = 5; price = 5000; };
+class CUP_launch_M136 { quality = 5; price = 5000; };
+class CUP_launch_9K32Strela { quality = 5; price = 5000; };
 
 // CUP_AT
-class CUP_launch_Javelin { quality = 6; price = 5000; };
-class CUP_launch_M47 { quality = 6; price = 5000; };
-class CUP_launch_MAAWS { quality = 6; price = 5000; };
-class CUP_launch_MAAWS_Scope { quality = 6; price = 5000; };
-class CUP_launch_Metis { quality = 6; price = 5000; };
-class CUP_launch_RPG18 { quality = 6; price = 5000; };
+class CUP_launch_Javelin { quality = 5; price = 5000; };
+class CUP_launch_M47 { quality = 5; price = 5000; };
+class CUP_launch_MAAWS { quality = 5; price = 5000; };
+class CUP_launch_MAAWS_Scope { quality = 5; price = 5000; };
+class CUP_launch_Metis { quality = 5; price = 5000; };
+class CUP_launch_RPG18 { quality = 5; price = 5000; };
