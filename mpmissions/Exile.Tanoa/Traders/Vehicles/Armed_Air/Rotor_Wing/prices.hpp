f42dc16ae1bf6e3a8d87b5f34311f9a051fa2519
AkToker <34366112+AkToker@users.noreply.github.com>
2017-12-13 17:51:51 -0900

    Cleaned/organised armed rotor

--- mpmissions/Exile.Tanoa/Traders/Vehicles/Armed_Air/Rotor_Wing/prices.hpp
+++ mpmissions/Exile.Tanoa/Traders/Vehicles/Armed_Air/Rotor_Wing/prices.hpp
@@ -1,41 +1,89 @@
 	// Completed by: AkToker 12/8/17
-	///////////////////////////////////////////////////////////////////////////////
-	// CUP Vehicles
-	///////////////////////////////////////////////////////////////////////////////
-	
-        class CUP_B_AH1Z_USMC							{ quality = 6; price = 450000; };
-        class CUP_B_AH1Z_Escort_USMC			                        { quality = 6; price = 450000; };
-        class CUP_B_AH64D_ES_USA					        { quality = 6; price = 450000; };
-        class CUP_B_AH64D_MR_USA						{ quality = 6; price = 450000; };
-        class CUP_B_AW159_Hellfire_GB						{ quality = 5; price = 350000; };
-        class CUP_B_AW159_Cannon_GB						{ quality = 5; price = 350000; };
-        class CUP_B_AW159_Hellfire_RN_Grey					{ quality = 5; price = 350000; };
-        class CUP_B_AW159_Cannon_RN_Grey					{ quality = 5; price = 350000; };
-        class CUP_B_AW159_Hellfire_RN_Blackcat					{ quality = 5; price = 350000; };
-        class CUP_B_AW159_Cannon_RN_Blackcat					{ quality = 5; price = 350000; };
-        class CUP_B_CH47F_USA							{ quality = 4; price = 200000; };
-        class CUP_B_CH47F_VIV_USA						{ quality = 4; price = 200000; };
-        class CUP_B_CH47F_GB							{ quality = 4; price = 200000; };
-        class CUP_B_CH47F_VIV_GB						{ quality = 4; price = 200000; };
-        class CUP_B_MH60S_USMC							{ quality = 4; price = 200000; };
-        class CUP_B_Mi24_D_CDF							{ quality = 6; price = 450000; };
-        class CUP_B_Mi35_CZ							{ quality = 6; price = 450000; };
-        class CUP_B_Mi35_CZ_Des							{ quality = 6; price = 450000; };
-        class CUP_B_Mi35_CZ_Ram							{ quality = 6; price = 450000; };
-        class CUP_B_Mi35_CZ_Tiger						{ quality = 6; price = 450000; };
-        class CUP_B_Mi35_CZ_Dark						{ quality = 6; price = 450000; };
-        class CUP_B_Mi35_CZ_Dark						{ quality = 6; price = 450000; };
-        class CUP_B_Mi17_CDF                                                    { quality = 4; price = 200000; };
-        class CUP_B_Mi171Sh_ACR							{ quality = 5; price = 400000; };
-        class CUP_B_Merlin_HC3_Armed_GB						{ quality = 4; price = 200000; };
-        class CUP_B_Merlin_HC3A_Armed_GB				        { quality = 4; price = 200000; };
-        class CUP_B_UH1Y_GUNSHIP_USMC			                        { quality = 5; price = 250000; };
-	class Exile_Chopper_Huey_Armed_Green					{ quality = 4; price = 150000; };
-	class Exile_Chopper_Huey_Armed_Desert					{ quality = 4; price = 150000; };
-        class CUP_B_UH60M_FFV_US                                                { quality = 4; price = 200000; };
-        class CUP_B_UH60L_FFV_US                                                { quality = 4; price = 200000; };
-        class CUP_B_AH6J_Escort_USA                                             { quality = 5; price = 300000; };
-        class CUP_B_AH6J_AT_USA                             	                { quality = 5; price = 300000; };
-        class CUP_B_AH6J_MP_USA                                 	        { quality = 5; price = 300000; };
-        class CUP_B_AH6J_Escort19_USA                                           { quality = 5; price = 300000; };
+
+///////////////////////////////////////////////////////////////////////////////
+// CUP Vehicles
+///////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+// AH-1Z Viper
+///////////////////////////////////////////////////////////////////////////////
+class CUP_B_AH1Z_USMC						   { quality = 6; price = 450000; };
+class CUP_B_AH1Z_Escort_USMC			                   { quality = 6; price = 450000; };
+
+///////////////////////////////////////////////////////////////////////////////
+// AH-64D Apache
+///////////////////////////////////////////////////////////////////////////////
+class CUP_B_AH64D_ES_USA					   { quality = 6; price = 450000; };
+class CUP_B_AH64D_MR_USA					   { quality = 6; price = 450000; };
+
+///////////////////////////////////////////////////////////////////////////////
+// AW-159 Wildcat/Hellcat
+///////////////////////////////////////////////////////////////////////////////
+class CUP_B_AW159_Hellfire_GB					   { quality = 5; price = 350000; };
+class CUP_B_AW159_Cannon_GB					   { quality = 5; price = 350000; };
+class CUP_B_AW159_Hellfire_RN_Grey				   { quality = 5; price = 350000; };
+class CUP_B_AW159_Cannon_RN_Grey				   { quality = 5; price = 350000; };
+class CUP_B_AW159_Hellfire_RN_Blackcat				   { quality = 5; price = 350000; };
+class CUP_B_AW159_Cannon_RN_Blackcat				   { quality = 5; price = 350000; };
+
+///////////////////////////////////////////////////////////////////////////////
+// CH-47F Chinook
+///////////////////////////////////////////////////////////////////////////////
+class CUP_B_CH47F_USA						   { quality = 4; price = 200000; };
+class CUP_B_CH47F_VIV_USA					   { quality = 4; price = 200000; };
+class CUP_B_CH47F_GB						   { quality = 4; price = 200000; };
+class CUP_B_CH47F_VIV_GB					   { quality = 4; price = 200000; };
+
+///////////////////////////////////////////////////////////////////////////////
+// MH-60S Seahawk
+///////////////////////////////////////////////////////////////////////////////
+class CUP_B_MH60S_USMC						   { quality = 4; price = 200000; };
+
+///////////////////////////////////////////////////////////////////////////////
+// Mi-24
+///////////////////////////////////////////////////////////////////////////////
+class CUP_B_Mi24_D_CDF						   { quality = 6; price = 450000; };
+
+///////////////////////////////////////////////////////////////////////////////
+// Mi-35 Hind
+///////////////////////////////////////////////////////////////////////////////
+class CUP_B_Mi35_CZ						   { quality = 6; price = 450000; };
+class CUP_B_Mi35_CZ_Des						   { quality = 6; price = 450000; };
+class CUP_B_Mi35_CZ_Ram						   { quality = 6; price = 450000; };
+class CUP_B_Mi35_CZ_Tiger					   { quality = 6; price = 450000; };
+class CUP_B_Mi35_CZ_Dark					   { quality = 6; price = 450000; };
+class CUP_B_Mi35_CZ_Dark					   { quality = 6; price = 450000; };
+
+///////////////////////////////////////////////////////////////////////////////
+// Mi-17
+///////////////////////////////////////////////////////////////////////////////
+class CUP_B_Mi17_CDF                                               { quality = 4; price = 200000; };
+class CUP_B_Mi171Sh_ACR						   { quality = 5; price = 400000; };
+
+///////////////////////////////////////////////////////////////////////////////
+// HC-3 Merlin
+///////////////////////////////////////////////////////////////////////////////
+class CUP_B_Merlin_HC3_Armed_GB					   { quality = 4; price = 200000; };
+class CUP_B_Merlin_HC3A_Armed_GB				   { quality = 4; price = 200000; };
+
+///////////////////////////////////////////////////////////////////////////////
+// UH-1Y Venom / Huey
+///////////////////////////////////////////////////////////////////////////////
+class CUP_B_UH1Y_GUNSHIP_USMC			                   { quality = 5; price = 250000; };
+class Exile_Chopper_Huey_Armed_Green				   { quality = 4; price = 150000; };
+class Exile_Chopper_Huey_Armed_Desert				   { quality = 4; price = 150000; };
+
+///////////////////////////////////////////////////////////////////////////////
+// UH-60 Blackhawk
+///////////////////////////////////////////////////////////////////////////////
+class CUP_B_UH60M_FFV_US                                           { quality = 4; price = 200000; };
+class CUP_B_UH60L_FFV_US                                           { quality = 4; price = 200000; };
+
+///////////////////////////////////////////////////////////////////////////////
+// AH-6J Little Bird
+///////////////////////////////////////////////////////////////////////////////
+class CUP_B_AH6J_Escort_USA                                        { quality = 5; price = 300000; };
+class CUP_B_AH6J_AT_USA                             	           { quality = 5; price = 300000; };
+class CUP_B_AH6J_MP_USA                                 	   { quality = 5; price = 300000; };
+class CUP_B_AH6J_Escort19_USA                                      { quality = 5; price = 300000; };
 
