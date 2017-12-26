d5724dff6420fb3d86ebff579d66d6d198a279bb
silentghoust <matthewperkins880@gmail.com>
2017-12-25 20:47:31 -0800

    ADDED AMMO

--- mpmissions/Exile.Tanoa/Traders/Items/Special_Operations/Ammunition/traders.hpp
+++ mpmissions/Exile.Tanoa/Traders/Items/Special_Operations/Ammunition/traders.hpp
@@ -4,8 +4,29 @@ class Launcher_Ammo
 	icon = "a3\ui_f\data\gui\Rsc\RscDisplayArsenal\cargothrow_ca.paa";
 	items[] =
 	{
-		"HandGrenade",
-		"MiniGrenade",
+		"CUP_PG7V_M",
+		"CUP_PG7VM_M",
+		"CUP_PG7VL_M",
+		"CUP_PG7VR_M",
+		"CUP_OG7_M",
+		"CUP_TBG7V_M",
+		"CUP_Javelin_M",
+		"CUP_Igla_M",
+		"CUP_MAAWS_HEAT_M",
+		"CUP_MAAWS_HEDP_M",
+		"CUP_SMAW_HEAA_M",
+		"CUP_SMAW_HEDP_M",
+		"CUP_SMAW_Spotting",
+		"CUP_M72A6_M",
+		"RPG32_F",
+		"RPG32_HE_F",
+		"Titan_AA",
+		"Titan_AT"
+
+
+
+
+
 		
 	};
 };
