5da9413ddd538d93d32e8985fc5475691d300444
Zataralee <Zataralee@gmail.com>
2017-12-26 12:51:09 -0800

    Formatting and fixes

--- mpmissions/Exile.Tanoa/Traders/Items/Special_Operations/Launchers/prices.hpp
+++ mpmissions/Exile.Tanoa/Traders/Items/Special_Operations/Launchers/prices.hpp
@@ -12,10 +12,22 @@ class CUP_launch_M136			{ quality = 3; price = 5000; };
 class CUP_launch_9K32Strela		{ quality = 4; price = 5000; };
 
 // CUP_AT
-class CUP_launch_Javelin		{ quality = 5; price = 5000; };
-class CUP_launch_M47			{ quality = 5; price = 5000; };
-class CUP_launch_MAAWS			{ quality = 3; price = 5000; };
-class CUP_launch_MAAWS_Scope	{ quality = 5; price = 5000; };
-class CUP_launch_Metis			{ quality = 5; price = 5000; };
-class CUP_launch_RPG18			{ quality = 4; price = 5000; };
-class CUP_launch_RPG7V			{ quality = 2; price = 1000; };
+class CUP_launch_Javelin				{ quality = 5; price = 5000; };
+class CUP_launch_M47					{ quality = 5; price = 5000; };
+class CUP_launch_MAAWS					{ quality = 3; price = 5000; };
+class CUP_launch_MAAWS_Scope			{ quality = 5; price = 5000; };
+class CUP_launch_Metis					{ quality = 5; price = 5000; };
+class CUP_launch_RPG18					{ quality = 4; price = 5000; };
+class CUP_launch_RPG7V					{ quality = 2; price = 1000; };
+
+// CUP added by Sabre
+class CUP_Weapon_launch_Mk153Mod0		{ quality = 4; price = 4000; };
+class CUP_Weapon_launch_M72A6_Special	{ quality = 5; price = 5000; };
+class Weapon_launch_RPG32_ghex_F		{ quality = 5; price = 5000; };
+class CUP_Weapon_launch_Igla			{ quality = 5; price = 5000; };
+class CUP_Weapon_launch_Metis			{ quality = 5; price = 5000; };
+class CUP_Weapon_launch_FIM92Stinger	{ quality = 5; price = 5000; };
+class Weapon_launch_NLAW_F				{ quality = 5; price = 5000; };
+class CUP_Weapon_launch_Javelin			{ quality = 5; price = 5000; };
+class Weapon_launch_I_Titan_F			{ quality = 5; price = 5000; };
+class Weapon_launch_I_Titan_short_F		{ quality = 5; price = 5000; };
