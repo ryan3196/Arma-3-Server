467f6ed25796758323df323d330803fe61445192
Zataralee <Zataralee@gmail.com>
2017-12-25 20:28:03 -0800

    For Sabre

---
+++ mpmissions/Exile.Tanoa/Traders/Items/Special_Operations/Ammunition/prices.hpp
@@ -0,0 +1,23 @@
+	///////////////////////////////////////////////////////////////////////////////
+	// Explosives
+	///////////////////////////////////////////////////////////////////////////////
+	class HandGrenade								{ quality = 3; price = 40; };
+	class MiniGrenade								{ quality = 2; price = 30; };
+	class B_IR_Grenade								{ quality = 1; price = 50; };
+	class O_IR_Grenade								{ quality = 1; price = 50; };
+	class I_IR_Grenade								{ quality = 1; price = 50; };
+	class 1Rnd_HE_Grenade_shell						{ quality = 2; price = 70; };
+	class 3Rnd_HE_Grenade_shell						{ quality = 3; price = 70*3; };
+	class APERSBoundingMine_Range_Mag				{ quality = 3; price = 500; };
+	class APERSMine_Range_Mag						{ quality = 3; price = 600; };
+	class APERSTripMine_Wire_Mag					{ quality = 3; price = 300; };
+	class APERSMineDispenser_Mag					{ quality = 5; price = 10000; };
+	class TrainingMine_Mag							{ quality = 5; price = 100; };
+	class ClaymoreDirectionalMine_Remote_Mag		{ quality = 3; price = 350; };
+	class DemoCharge_Remote_Mag						{ quality = 4; price = 700; };
+	class IEDLandBig_Remote_Mag						{ quality = 3; price = 600; };
+	class IEDLandSmall_Remote_Mag					{ quality = 3; price = 300; };
+	class IEDUrbanBig_Remote_Mag					{ quality = 3; price = 800; };
+	class IEDUrbanSmall_Remote_Mag					{ quality = 3; price = 400; };
+	class SatchelCharge_Remote_Mag					{ quality = 5; price = 1000; };
+	class SLAMDirectionalMine_Wire_Mag				{ quality = 3; price = 700; };
---
+++ mpmissions/Exile.Tanoa/Traders/Items/Special_Operations/Ammunition/traders.hpp
@@ -0,0 +1,11 @@
+class Launcher_Ammo
+{
+	name = "Launcher Ammo";
+	icon = "a3\ui_f\data\gui\Rsc\RscDisplayArsenal\cargothrow_ca.paa";
+	items[] =
+	{
+		"HandGrenade",
+		"MiniGrenade",
+		
+	};
+};
