de98d3db314a10ebec5e44e83f3c3f5e60e0f80e
Zataralee <Zataralee@gmail.com>
2017-12-26 12:34:48 -0800

    Sabre was tired

--- mpmissions/Exile.Tanoa/Traders/Items/Special_Operations/Ammunition/prices.hpp
+++ mpmissions/Exile.Tanoa/Traders/Items/Special_Operations/Ammunition/prices.hpp
@@ -1,23 +1,22 @@
 	///////////////////////////////////////////////////////////////////////////////
-	// Explosives
+	// Launcher Ammo
 	///////////////////////////////////////////////////////////////////////////////
-class HandGrenade								{ quality = 3; price = 100; };
-	class CUP_PG7V_M								{ quality = 3; price = 100; };
-	class CUP_PG7VM_M								{ quality = 3; price = 100; };
-	class CUP_PG7VL_M								{ quality = 3; price = 100; };
-	class CUP_PG7VR_M								{ quality = 3; price = 100; };
-	class CUP_OG7_M						{ quality =32; price = 100; };
-	class CUP_TBG7V_M						{ quality = 3; price = 100; };
-	class CUP_Javelin_M				{ quality = 4; price = 500; };
-	class CUP_Igla_M						{ quality = 4; price = 600; };
-	class CUP_MAAWS_HEAT_M					{ quality = 4; price = 300; };
-	class CUP_MAAWS_HEDP_M					{ quality = 4; price = 10000; };
-	class CUP_SMAW_HEAA_M							{ quality = 4; price = 100; };
-	class CUP_SMAW_HEDP_M		{ quality = 4; price = 350; };
-	class CUP_SMAW_Spotting						{ quality = 4; price = 700; };
-	class CUP_M72A6_M						{ quality = 4; price = 600; };
-	class RPG32_F					{ quality = 4; price = 300; };
-	class RPG32_HE_F					{ quality = 4; price = 800; };
-	class Titan_AA					{ quality = 4; price = 400; };
-	class Titan_AT					{ quality = 5; price = 1000; };
-	class SLAMDirectionalMine_Wire_Mag				{ quality = 4; price = 700; };
+	class CUP_PG7V_M								{ quality = 3; price = 1000; };
+	class CUP_PG7VM_M								{ quality = 3; price = 1000; };
+	class CUP_PG7VL_M								{ quality = 3; price = 1000; };
+	class CUP_PG7VR_M								{ quality = 3; price = 1000; };
+	class CUP_OG7_M									{ quality = 3; price = 1000; };
+	class CUP_TBG7V_M								{ quality = 3; price = 1000; };
+	class CUP_Javelin_M								{ quality = 4; price = 5000; };
+	class CUP_Igla_M								{ quality = 4; price = 6000; };
+	class CUP_MAAWS_HEAT_M							{ quality = 4; price = 3000; };
+	class CUP_MAAWS_HEDP_M							{ quality = 4; price = 10000; };
+	class CUP_SMAW_HEAA_M							{ quality = 4; price = 1000; };
+	class CUP_SMAW_HEDP_M							{ quality = 4; price = 3500; };
+	class CUP_SMAW_Spotting							{ quality = 4; price = 7000; };
+	class CUP_M72A6_M								{ quality = 4; price = 6000; };
+	class RPG32_F									{ quality = 4; price = 3000; };
+	class RPG32_HE_F								{ quality = 4; price = 8000; };
+	class Titan_AA									{ quality = 4; price = 4000; };
+	class Titan_AT									{ quality = 5; price = 1000; };
+	
